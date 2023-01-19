0fdffe5f1d18c96dfb44255a2dda1cb70728eace
bilalerdemm <bilalerdemm96@gmail.com>
2023-01-19 18:57:58 +0300

    1 saniyede bir tane ekmek uretme olayı cozuldu

--- Assets/Scripts/PathMovement.cs
+++ Assets/Scripts/PathMovement.cs
@@ -8,6 +8,11 @@ public class PathMovement : MonoBehaviour
     public PathCreator pathCreator;
     public float speed = 5f;
     float distanceTravelled;
+
+    public int currentDough;
+    public bool giveDough;
+
+    public float timer = 1f;
     void Update()
     {
         if (Input.GetMouseButton(0))
@@ -15,12 +20,28 @@ public class PathMovement : MonoBehaviour
             distanceTravelled += speed * Time.deltaTime;
             transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled) + new Vector3(0, 4, 0);
         }
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        if (timer <= 0)
+        {
+            giveDough = true;
+        }
     }
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag("DoughStack"))
+        if (other.gameObject.CompareTag("DoughStack") && currentDough <= 4 && giveDough)
         {
-            Debug.Log("Hamur stackle");
+            DoughStackMechanic();
         }
+    }
+    void DoughStackMechanic()
+    {
+        giveDough = false;
+        timer = 1f;
+        currentDough++;
+        Debug.Log("Hamur stackle");
+        //StartCoroutine(DoughStackMechanic());
     }
 }
